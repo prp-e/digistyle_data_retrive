2c7ab7700b35ebdc5ccd80265beb836ff22f5d1c
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-08-13 23:33:57 +0430

    Now it iterates among 10 pages of Digistyle

--- crawler.py
+++ crawler.py
@@ -1,20 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
 products = []
-base_url = "https://www.digistyle.com/plp/plp_29733814/?pageno=4"
-res = requests.get(base_url)
+for i in range(10):
+    base_url = f"https://www.digistyle.com/plp/plp_29733814/?pageno={i + 1}"
+    res = requests.get(base_url)
 
-crawled_data = BeautifulSoup(res.text, "html.parser")
-data = crawled_data.find_all("div", {"class": "cp-card cp-card--product-card js-cro-product"})
+    crawled_data = BeautifulSoup(res.text, "html.parser")
+    data = crawled_data.find_all("div", {"class": "cp-card cp-card--product-card js-cro-product"})
 
-for datum in data:
-    picture = datum.find("img", {"class": "c-product-card__image"})["src"]
-    url = datum.find("a", {"c-product-card__image-container ga-product-impression"})["href"]
-    product = {
-        "picture": picture,
-        "url": f'https://www.digistyle.com{url}'
-    }
-    products.append(product)
+    for datum in data:
+        picture = datum.find("img", {"class": "c-product-card__image"})["src"]
+        url = datum.find("a", {"c-product-card__image-container ga-product-impression"})["href"]
+        product = {
+            "picture": picture,
+            "url": f'https://www.digistyle.com{url}'
+        }
+        products.append(product)
+    
+    time.sleep(10)
 
 print(products)
