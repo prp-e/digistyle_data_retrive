117b0ea8f352ddca211ebaf849b9b3033a1f7eed
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-08-13 23:26:14 +0430

    We've got each product on a single page

--- crawler.py
+++ crawler.py
@@ -4,4 +4,7 @@ import requests
 base_url = "https://www.digistyle.com/plp/plp_29733814/?pageno=4"
 res = requests.get(base_url)
 
-print(res.text)
+crawled_data = BeautifulSoup(res.text, "html.parser")
+data = crawled_data.find_all("div", {"class": "cp-card cp-card--product-card js-cro-product"})
+
+print(data)
