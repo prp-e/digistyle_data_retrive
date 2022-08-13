8e9df1c47e1afc38cb8dcf2f21109a927c6541cb
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-08-13 23:30:44 +0430

    Now we're going to make it for multiple pages, then store all crawled data in JSON file

--- crawler.py
+++ crawler.py
@@ -1,10 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
+products = []
 base_url = "https://www.digistyle.com/plp/plp_29733814/?pageno=4"
 res = requests.get(base_url)
 
 crawled_data = BeautifulSoup(res.text, "html.parser")
 data = crawled_data.find_all("div", {"class": "cp-card cp-card--product-card js-cro-product"})
 
-print(data)
+for datum in data:
+    picture = datum.find("img", {"class": "c-product-card__image"})["src"]
+    url = datum.find("a", {"c-product-card__image-container ga-product-impression"})["href"]
+    product = {
+        "picture": picture,
+        "url": f'https://www.digistyle.com{url}'
+    }
+    products.append(product)
+
+print(products)
